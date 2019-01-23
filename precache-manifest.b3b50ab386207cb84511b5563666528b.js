d73d565c6600e607b13a9c5599531cb248573429
Moses Surumen <moses.susrumen@gmail.com>
2019-01-23 01:48:01 -0800

    Updates

---
+++ precache-manifest.b3b50ab386207cb84511b5563666528b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "381e3e15d3be97bab48090cc25a4c38d",
+    "url": "/surumen/surumen/static/media/sound.381e3e15.svg"
+  },
+  {
+    "revision": "f75d8afa932636de2d4c",
+    "url": "/surumen/surumen/static/js/runtime~main.f75d8afa.js"
+  },
+  {
+    "revision": "0665843b359970ecd582",
+    "url": "/surumen/surumen/static/js/main.0665843b.chunk.js"
+  },
+  {
+    "revision": "9e8b9bf79cb2c834d4f0",
+    "url": "/surumen/surumen/static/js/1.9e8b9bf7.chunk.js"
+  },
+  {
+    "revision": "0665843b359970ecd582",
+    "url": "/surumen/surumen/static/css/main.e7efc2b3.chunk.css"
+  },
+  {
+    "revision": "7ff7f588afc1757b9b4d7fa3cc94271e",
+    "url": "/surumen/surumen/index.html"
+  }
+];
